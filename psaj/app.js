dfa88b8451daef4fa8f1fd62ead7cf1958ab8c0b
alfisyusuf <alfisyusuf@gmail.com>
2023-04-08 05:21:15 +0700

    upd

--- psaj/app.js
+++ psaj/app.js
@@ -2,7 +2,7 @@ function login() {
   var password = document.getElementById("password").value;
 
   // Ganti password di bawah dengan password yang diinginkan
-  if (password === "passwordku") {
+  if (password === "bertumbuh") {
     window.location.href = "susulan897.html";
   } else {
     alert("Password salah. Silakan coba lagi.");
