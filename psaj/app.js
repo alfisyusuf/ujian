8aca8738b68be38d031374a656002e26b0b1a823
alfisyusuf <73928304+alfisyusuf@users.noreply.github.com>
2023-04-07 21:11:41 +0700

    Update app.js

--- psaj/app.js
+++ psaj/app.js
@@ -3,7 +3,7 @@ function login() {
 
   // Ganti password di bawah dengan password yang diinginkan
   if (password === "passwordku") {
-    window.location.href = "homepage.html";
+    window.location.href = "susulan897.html";
   } else {
     alert("Password salah. Silakan coba lagi.");
   }
