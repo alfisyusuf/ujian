4835e0117aa3d909a6d0d57b91cf19041a201d40
alfisyusuf <73928304+alfisyusuf@users.noreply.github.com>
2023-04-08 09:54:12 +0700

    Update app.js

--- psaj/app.js
+++ psaj/app.js
@@ -2,7 +2,7 @@ function login() {
   var password = document.getElementById("password").value;
 
   // Ganti password di bawah dengan password yang diinginkan
-  if (password === "kemajuan") {
+  if (password === "berakhlak") {
     window.location.href = "susulan897.html";
   } else {
     alert("Password salah. Silakan coba lagi.");
